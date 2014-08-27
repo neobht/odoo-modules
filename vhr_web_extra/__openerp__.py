d709b8360d424aece3d97be00060cd99ca0161c9
Anton Goroshkin <antihaos@gmail.com>
2014-08-27 17:20:07 +0800

    add vhr_web_extra & disable_openerp_online modules

---
+++ vhr_web_extra/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Odoo Web Extra',
+    'version': '1.0',
+    'category': 'web',
+    'complexity': "easy",
+    'description': """
+Extra web feature
+=================
+* Fold and unfold left menu
+    """,
+    'author': 'giaosudau',
+    'website': 'http://openerp.com',
+    'depends': ['web'],
+    'installable': True,
+    'active': False,
+    'data': ['views/extra.xml', ],
+    'qweb': [
+        'static/src/xml/template',
+    ],
+}
